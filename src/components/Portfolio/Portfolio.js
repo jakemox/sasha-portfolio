16d2e4c1a5a56a6a3e2ffabd5caf042a40093143
Jake Moxon <jake.moxon@luxusworldwide.com>
2021-11-10 17:21:14 +0000

    new images

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,43 +5,38 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core'
 import { SRLWrapper } from 'simple-react-lightbox'
 
-// const SeventiesFlower = lazy(() => import('../../assets/images/70s-flower.jpg'));
-// const Accessories = lazy(() => import('../../assets/images/accessories.jpg'));
-// const Assignment = lazy(() => import('../../assets/images/assignment.jpg'));
-// const Bird = lazy(() => import('../../assets/images/bird.jpg'));
-// const BlueFlower = lazy(() => import('../../assets/images/blue-flower.jpg'));
-// const BobAndBug = lazy(() => import('../../assets/images/bob-and-bug.jpg'));
-// const BobCover = lazy(() => import('../../assets/images/bob-cover.jpg'));
-// const Butterfly = lazy(() => import('../../assets/images/butterfly.jpg'));
-// const CharacterSheet = lazy(() => import('../../assets/images/character-sheet.jpg'));
-// const Daisies = lazy(() => import('../../assets/images/daisies.jpg'));
-// const DaisyMoon = lazy(() => import('../../assets/images/daisy-moon.jpg'));
-// const Marigold1 = lazy(() => import('../../assets/images/marigold-1.jpg'));
-// const PaisleyFlower = lazy(() => import('../../assets/images/paisley-flower.jpg'));
-// const PartridgeCard = lazy(() => import('../../assets/images/partridge-card.jpg'));
-// const Poses = lazy(() => import('../../assets/images/poses.jpg'));
-// const Robin = lazy(() => import('../../assets/images/robin.jpg'));
-// const Tortoise = lazy(() => import('../../assets/images/tortoise.jpg'));
-
 import SeventiesFlower from '../../assets/images/70s-flower.jpg'
-import Accessories from '../../assets/images/accessories.jpg'
-import Assignment from '../../assets/images/assignment.jpg'
 import Bird from '../../assets/images/bird.jpg'
+import BirthdayFlower from '../../assets/images/birthday-flower.jpg'
 import BlueFlower from '../../assets/images/blue-flower.jpg'
 import BobAndBug from '../../assets/images/bob-and-bug.jpg'
 import BobCover from '../../assets/images/bob-cover.jpg'
 import Butterfly from '../../assets/images/butterfly.jpg'
-import CharacterSheet from '../../assets/images/character-sheet.jpg'
+import CharacterSheet from '../../assets/images/seed-book-character-sheet.jpg'
 import Daisies from '../../assets/images/daisies.jpg'
-import DaisyMoon from '../../assets/images/daisy-moon.jpg'
+import Emotions from '../../assets/images/emotions.jpg'
 import Marigold1 from '../../assets/images/marigold-1.jpg'
+import Marigold2 from '../../assets/images/marigold-2.jpg'
+import MumFlower from '../../assets/images/mum-flower.jpg'
+import PaintPurple from '../../assets/images/paint-purple-spread.jpg'
 import PaisleyFlower from '../../assets/images/paisley-flower.jpg'
 import PartridgeCard from '../../assets/images/partridge-card.jpg'
 import Poses from '../../assets/images/poses.jpg'
 import Robin from '../../assets/images/robin.jpg'
-import Tortoise from '../../assets/images/tortoise.jpg'
+import SashaPurple from '../../assets/images/sasha-purple.jpg'
+import WildFlowers from '../../assets/images/wild-flowers.jpg'
 
 const portfolioItems = [
+  {
+    columns: 7,
+    image: PaintPurple,
+    alt: 'I Paint Purple Spread',
+  },
+  {
+    columns: 5,
+    image: SashaPurple,
+    alt: 'Sasha Purple',
+  },
   {
     columns: 4,
     image: Poses,
@@ -58,16 +53,6 @@ const portfolioItems = [
     alt: 'Partridge Card',
   },
   {
-    columns: 6,
-    image: CharacterSheet,
-    alt: 'Character Sheet',
-  },
-  {
-    columns: 6,
-    image: Assignment,
-    alt: 'Assignment',
-  },
-  {
     columns: 8,
     image: BobAndBug,
     alt: 'Bob and Bug',
@@ -78,44 +63,19 @@ const portfolioItems = [
     alt: 'Daisies',
   },
   {
+    columns: 6,
+    image: CharacterSheet,
+    alt: 'Seed Book Character Sheet',
+  },
+  {
+    columns: 6,
+    image: Emotions,
+    alt: 'Emotions',
+  },
+  {
     columns: 5,
     image: Robin,
     alt: 'Robin',
-  },
-  {
-    columns: 7,
-    image: Butterfly,
-    alt: 'Butterfly',
-  },
-  {
-    columns: 7,
-    image: Tortoise,
-    alt: 'Tortoise',
-  },
-  {
-    columns: 5,
-    image: Accessories,
-    alt: 'Accessories',
-  },
-  {
-    columns: 4,
-    image: SeventiesFlower,
-    alt: 'Seventies Flower',
-  },
-  {
-    columns: 4,
-    image: DaisyMoon,
-    alt: 'Daisy Moon',
-  },
-  {
-    columns: 4,
-    image: Marigold1,
-    alt: 'Marigold 1',
-  },
-  {
-    columns: 5,
-    image: BlueFlower,
-    alt: 'Blue Flower',
   },
   {
     columns: 3,
@@ -126,6 +86,46 @@ const portfolioItems = [
     columns: 4,
     image: Bird,
     alt: 'Bird',
+  },
+  {
+    columns: 5,
+    image: BlueFlower,
+    alt: 'Blue Flower',
+  },
+  {
+    columns: 7,
+    image: Butterfly,
+    alt: 'Butterfly',
+  },
+  {
+    columns: 4,
+    image: Marigold2,
+    alt: 'Marigold',
+  },
+  {
+    columns: 4,
+    image: BirthdayFlower,
+    alt: 'Birthday Flower',
+  },
+  {
+    columns: 4,
+    image: Marigold1,
+    alt: 'Marigolds',
+  },
+  {
+    columns: 4,
+    image: SeventiesFlower,
+    alt: 'Seventies Flower',
+  },
+  {
+    columns: 4,
+    image: MumFlower,
+    alt: 'Flower',
+  },
+  {
+    columns: 4,
+    image: WildFlowers,
+    alt: 'Wild Flowers',
   },
 ]
 

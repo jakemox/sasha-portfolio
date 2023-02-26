15e95b84befbd2c6c50f459bf9cfc7f354975fa5
Jake Moxon <jake.moxon.ext@hmdglobal.com>
2023-02-26 11:50:17 +0000

    Optimisation for images

--- src/components/Portfolio/PortfolioImage.js
+++ src/components/Portfolio/PortfolioImage.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
   picture: {
@@ -25,13 +25,43 @@ const useStyles = makeStyles((theme) => ({
 
 const PortfolioImage = ({ src, alt, positionX, positionY }) => {
   const classes = useStyles({ positionX, positionY })
+  const theme = useTheme()
+
+  const breakpoint = (size) => theme.breakpoints.values[size]
+
+  const sizes = `(max-width: ${breakpoint('sm')}px) ${breakpoint(
+    'sm'
+  )}px, (max-width: ${breakpoint('lg')}px) ${breakpoint('lg')}px`
 
   return (
     <picture className={classes.picture}>
+      {/* <source
+        type='image/avif'
+        srcSet={`${src}?w=${breakpoint('sm')}&fm=avif ${breakpoint(
+          'sm'
+        )}w, ${src}?w=${breakpoint('lg')}&fm=avif ${breakpoint(
+          'lg'
+        )}w, ${src}?fm=avif`}
+        sizes={sizes}
+      /> */}
+      <source
+        type='image/webp'
+        srcSet={`${src}?w=${breakpoint('sm')}&fm=webp ${breakpoint(
+          'sm'
+        )}w, ${src}?w=${breakpoint('lg')}&fm=webp ${breakpoint(
+          'lg'
+        )}w, ${src}?fm=webp`}
+        sizes={sizes}
+      />
       <img
+        srcSet={`${src}?w=${breakpoint('sm')} ${breakpoint(
+          'sm'
+        )}, ${src}?w=${breakpoint('lg')} ${breakpoint('lg')}w, ${src}`}
+        sizes={`${sizes}, ${breakpoint('lg') + 1}px`}
         src={src}
         className={classes.image}
         alt={alt}
+        loading='lazy'
         onContextMenu={(e) => e.preventDefault()}
       />
     </picture>

259a0a9d888a56f7e6c5d4974acb61e6bcb2326c
Jake Moxon <jake_mox@hotmail.com>
2026-06-25 17:54:21 +0100

    Update apolloClient env logic

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,18 +1,18 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const createApolloClient = () => {
-  const isLocal = import.meta.env.DEV
+  const isProd = import.meta.env.PROD
 
   return new ApolloClient({
-    uri: isLocal
-      ? `https://graphql.contentful.com/content/v1/spaces/${import.meta.env.VITE_APP_CONTENTFUL_SPACE_ID}`
-      : '/.netlify/functions/fetchContentfulData',
-    headers: isLocal
-      ? {
+    uri: isProd
+      ? '/.netlify/functions/fetchContentfulData'
+      : `https://graphql.contentful.com/content/v1/spaces/${import.meta.env.VITE_APP_CONTENTFUL_SPACE_ID}`,
+    headers: isProd
+      ? undefined
+      : {
           Authorization: `Bearer ${import.meta.env.VITE_APP_CONTENTFUL_ACCESS_TOKEN}`,
           'Content-Type': 'application/json',
-        }
-      : undefined,
+        },
     cache: new InMemoryCache(),
   })
 }
